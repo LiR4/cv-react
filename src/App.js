0fabea768e712adeedd395e49c272a6fd3595321
LiR4 <pedrolira521@gmail.com>
2023-01-04 19:54:08 -0300

    Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import Header from "./Components/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="">
-      
+    <div className="App">
+      <div className="container">
+        <div class="a-w0nw1">
+          <Header />
+        </div>
+        <div class="a-MZ7AG">a-MZ7AG</div>
+        <div class="a-16VNL">a-16VNL</div>
+        <div class="a-jYAK1-0">a-jYAK1-0</div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,30 @@
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import Logo from "../../Assets/logo.png";
+
+import React from "react";
+
+export default function Header() {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="md">
+        <Container>
+          <Navbar.Brand href="#home">
+            <Image src={Logo} fluid/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto"></Nav>
+            <Nav>
+              <Nav.Link href="#Hab">Habilidades</Nav.Link>
+              <Nav.Link href="#Pro">Projetos</Nav.Link>
+              <Nav.Link href="#Cont">Contatos</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
