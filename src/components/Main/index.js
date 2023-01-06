1c3cb90d835414f7cc14309b7b0b82bf11c18ac5
LiR4 <pedrolira521@gmail.com>
2023-01-06 16:42:25 -0300

    interfazce ok2

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,9 +4,7 @@ import Perfil from "../../Assets/perfil.jpeg";
 import Slide from "../Slide";
 import Slide2 from "../Slide2";
 
-
 import { Image } from "react-bootstrap";
-
 
 export default function Main() {
   return (
@@ -17,15 +15,19 @@ export default function Main() {
         </div>
         <div class="a-pZ9mM">
           <p>
-            Olá como vai. Me chamo Pedro, mas pode me chamar de Lira. Clique
-            aqui para ver a carta de apresentação.
+            Olá como vai. Me chamo Pedro, mas pode me chamar de Lira.{" "}
+            <a href="https://www.canva.com/design/DAFHus72YDE/DkLdGBHUpnVdP9wvCEb7rw/view?utm" target="_blank">Clique aqui</a> para ver a carta de apresentação.
           </p>
         </div>
         <div class="a-K8Ay4">
           <Image src={Perfil} roundedCircle />
         </div>
-        <div class="a-r2goO"><Slide /></div>
-        <div class="a-83XNQ"><Slide2/> </div>
+        <div class="a-r2goO">
+          <Slide />
+        </div>
+        <div class="a-83XNQ">
+          <Slide2 />{" "}
+        </div>
       </section>
     </div>
   );
